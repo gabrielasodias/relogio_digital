c6ce5e406aad0e3ec44e84f9d0312c13ec59b353
Gabriela Dias <gabrieladiasofc@gmail.com>
2023-12-16 15:12:09 -0300

    Criada a função para manipulação das horas

--- relogio_digital.py
+++ relogio_digital.py
@@ -4,3 +4,9 @@ from time import strftime #biblioteca e função para informações das horas
 
 janela = Tk() #instância para manipulação da janela
 janela.title('Relógio Digital') #título da janela
+
+#função para manipulação das horas
+def time():
+	string = strftime('%H:%M:%S %p') #tempo formatado
+	lbl.config(text = string) #configuração do texto
+	lbl.after(1000, time) #manipulação do tempo
