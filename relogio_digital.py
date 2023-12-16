f852006434790df76f5bab94dab9e69cc6b5431f
Gabriela Dias <gabrieladiasofc@gmail.com>
2023-12-16 15:03:40 -0300

    Configuração inicial da janela

--- relogio_digital.py
+++ relogio_digital.py
@@ -1,3 +1,6 @@
 from tkinter import * #biblioteca  de interface
 from tkinter.ttk import * #módulo de widgets
 from time import strftime #biblioteca e função para informações das horas
+
+janela = Tk() #instância para manipulação da janela
+janela.title('Relógio Digital') #título da janela
